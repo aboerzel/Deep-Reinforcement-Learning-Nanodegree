71e372c9afc901230933c3610e6e0090da35037c
Andreas <andreas.boerzel@gmx.de>
2021-01-29 14:35:02 +0100

    start scripts

--- start-conda.bat
+++ start-conda.bat
@@ -1,5 +1,5 @@
 d:
-cd d:\development\rlnd\deep-reinforcement-learning
-call activate drlnd
+cd d:\development\drlnd\deep-reinforcement-learning
+call conda activate drlnd
 call jupyter notebook
 pause
